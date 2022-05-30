8b53e107df026e22a80175888bc35bee13f84e7a
Mario <mario.gonzalez@hotmail.com.ar>
2022-05-30 10:44:48 -0300

    fondo rojo

--- calculadora.js
+++ calculadora.js
@@ -1,4 +1,20 @@
 function abre_calculadora() {
     console.log("Hola calculadora de mario y monica ");
 
+try {
+    
+
+const div = document.createElement('div');
+div.id = 'calculadora';
+div.className = 'calculadora';
+div.setAttribute('style','position: absolute;top: 5%;left: 0%;width: 100%;height: 85%;align-items: center;background-color: red;');
+
+document.body.appendChild(div);//este inserta el
+
+
+
+
+} catch (error) {
+    
+}
 }
