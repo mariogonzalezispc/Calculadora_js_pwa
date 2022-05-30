aa136ce29d31f6a1e41eb9e6c21be2d4ecd784b1
mariogonzalezispc <mario.gonzalez@hotmail.com.ar>
2022-05-30 16:17:02 -0300

    teclas

--- calculadora.js
+++ calculadora.js
@@ -6,9 +6,21 @@ try {
 /* Creamos div de fondo de la calculadora e insertamos en el DOM   */
 /*-----------------------------------------------------------------*/
 const fondo = document.createElement('div');
-fondo.id = 'calculadora';
+fondo.id = 'fondo';
 fondo.className = 'calculadora';
-fondo.setAttribute('style','position: absolute;top: 5%;left: 2%;width: 95%;height: 85%;align-items: center;background-color: crimson; border-radius: 10px; border: 2px solid black; justify-content: center;');
+//fondo.setAttribute('style','position: absolute;top: 5%;left: 2%;width: 95%;height: 85%;align-items: center;background-color: crimson; border-radius: 10px; border: 2px solid black; justify-content: center;');
+let parametros = 
+    "position: absolute;"+
+    "top: 5%;"+
+    "left: 2%;"+
+    "width: 95%;"+
+    "height: 85%;"+
+    "align-items: center;"+
+    "background-color: crimson;"+
+    "border-radius: 10px;"+
+    "border: 2px solid black;"+
+    "justify-content: center;";
+fondo.setAttribute('style',parametros);
 document.body.appendChild(fondo);//este inserta el div en el body del HTML 
 console.log("El fondo esta cargado en el DOM " + fondo.isConnected); // retorna el estado del div en el DOM
 /*-----------------------------------------------------------------*/
@@ -17,6 +29,7 @@ console.log("El fondo esta cargado en el DOM " + fondo.isConnected); // retorna 
 const display = document.createElement('div');
 display.id = 'display';
 display.className = 'display';
+let parametros2 =
 display.setAttribute('style','position: absolute;top: 4%;left: 4%;width: 91%;height: 15%;align-items: right;background-color: orangered; border-radius: 10px; border: 2px solid black;');
 fondo.appendChild(display);//este inserta el display en el body del HTML 
 console.log("El display esta cargado en el DOM " + display.isConnected); // retorna el estado del display en el DOM
@@ -26,6 +39,7 @@ console.log("El display esta cargado en el DOM " + display.isConnected); // reto
 const teclado = document.createElement('div');
 teclado.id = 'teclado';
 teclado.className = 'teclado';
+let parametros3 =
 teclado.setAttribute('style','position: absolute;top: 22%;left: 4%;width: 91%;height: 74%;align-items: center; background-color: pink; border-radius: 10px; border: 2px solid black;');
 fondo.appendChild(teclado);//este inserta el display en el body del HTML 
 console.log("El teclado esta cargado en el DOM " + teclado.isConnected); // retorna el estado del display en el DOM
@@ -35,6 +49,7 @@ console.log("El teclado esta cargado en el DOM " + teclado.isConnected); // reto
 const digitos = document.createElement('input');
 digitos.id = 'digitos';
 digitos.className = 'teclado';
+let parametros4 =
 digitos.setAttribute('style','position: absolute;top: 3%;left: 1.4%;width: 95%;height: 85%;align-items: right;background-color: grey;border-radius: 10px;');
 display.appendChild(digitos);//este inserta el display en el body del HTML 
 console.log("El digito esta cargado en el DOM " + digitos.isConnected); // retorna el estado del display en el DOM
@@ -44,6 +59,7 @@ console.log("El digito esta cargado en el DOM " + digitos.isConnected); // retor
 const borra = document.createElement('button');
 borra.id = 'digitos';
 borra.className = 'teclado';
+let parametros5 =
 borra.setAttribute('style','position: absolute;top: 3%;left: 1.4%;width: 25%;height: 20%;align-items: right;background-color: grey;border-radius: 10px;');
 teclado.appendChild(borra);//este inserta el display en el body del HTML 
 console.log("El digito esta cargado en el DOM " + digitos.isConnected); // retorna el estado del display en el DOM
