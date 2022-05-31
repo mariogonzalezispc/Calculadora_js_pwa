255285b852dd546bae574f239f1ba073c97017ec
mario <mario.gonzalez@hotmail.com.ar>
2022-05-30 22:46:58 -0300

    vs

--- calculadora.js
+++ calculadora.js
@@ -57,9 +57,7 @@ let parametros3 =
     "background-color: pink;"+ 
     "border-radius: 10px;"+ 
     "border: 2px solid black;";
-
 teclado.setAttribute('style',parametros3);
-
 fondo.appendChild(teclado);//este inserta el display en el body del HTML 
 console.log("El teclado esta cargado en el DOM " + teclado.isConnected); // retorna el estado del display en el DOM
 /*-----------------------------------------------------------------*/
@@ -83,7 +81,6 @@ let parametros4 =
     "background-color: grey;"+
     "border-radius: 10px;";
 digitos.setAttribute('style',parametros4);
-
 display.appendChild(digitos);//este inserta el display en el body del HTML 
 console.log("El digito esta cargado en el DOM " + digitos.isConnected); // retorna el estado del display en el DOM
 /*-----------------------------------------------------------------*/
@@ -107,7 +104,6 @@ digito1.innerHTML = '1';//carga el texto del boton
 digito1.onclick = function() {
     digitos.value += '1';
 }
-
 teclado.appendChild(digito1);//este inserta el display en el body del HTML 
 console.log("La tecla digito1 esta cargada en el DOM " + digito1.isConnected); // retorna el estado del display en el DOM
 /*-----------------------------------------------------------------*/
@@ -132,8 +128,95 @@ label1.setAttribute('style',parametros5label);//carga los estilos del label
 label1.innerHTML = "1";
 digito1.appendChild(label1);//este inserta el display en el body del HTML 
 console.log("La tecla label1 esta cargada en el DOM " + label1.isConnected); // retorna el estado del display en el DOM
+/*-----------------------------------------------------------------*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*-----------------------------------------------------------------*/
+/* Creamos tecla clear del teclado e insertamos en el DOM */
+/*-----------------------------------------------------------------*/
+const clear = document.createElement('button');
+clear.id = 'clear';
+clear.className = 'botones';
+let parametrosclear =
+    "position: absolute;"+
+    "top: 1%;"+
+    "left: 75%;"+
+    "width: 23%;"+
+    "height: 19%;"+
+    "align-items: right;"+
+    "background-color: red;"+
+"box-shadow: 5px 5px 10px -3px;"+
+"border-radius: 15px;";
+clear.setAttribute('style',parametrosclear);//carga los estilos del boton
+clear.innerHTML = '';//carga el texto del boton
+clear.onclick = function() {
+    digitos.value = '';
+}
+teclado.appendChild(clear);//este inserta el display en el body del HTML 
+console.log("La tecla clear esta cargada en el DOM " + clear.isConnected); // retorna el estado del display en el DOM
+/*-----------------------------------------------------------------*/
+const clear1 = document.createElement('label');
+clear1.id = 'clear1';
+clear1.className = 'clear1';
+let parametrosclearlabel =
+    "position: absolute;"+
+    "top: 0%;"+
+    "left: 0%;"+
+    "width: 100%;"+
+    "height: 100%;"+
+    "user-select: none;"+
+    "align-items: center;"+
+    "font-size: 40px;"+
+    "text-align: center;"+
+    "line-height: 75px;"+
+    "font-weight: bold;"+
+    "justify-content: center;"+
+    "background-color: transparent;";
+clear1.setAttribute('style',parametrosclearlabel);//carga los estilos del label
+clear1.innerHTML = "C";
+clear.appendChild(clear1);//este inserta el display en el body del HTML 
+console.log("el label clear esta cargada en el DOM " + clear.isConnected); // retorna el estado del display en el DOM
+/*-----------------------------------------------------------------*/
+
+
+
+
 } catch (error) {
     alert("Error al cargar la calculadora " + error);
     
